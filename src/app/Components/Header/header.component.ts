fe782ed4ca61a8a2dea879118e30cd9baf304b6a
Asmaa Abd El-Nasser <fam22fam18@gmail.com>
2022-07-16 01:48:53 +0200

    simple edit

--- src/app/Components/Header/header.component.ts
+++ src/app/Components/Header/header.component.ts
@@ -10,15 +10,8 @@ import { AdminService } from '../../Services/admin.service';
 })
 export class HeaderComponent implements OnInit {
 
-<<<<<<< HEAD
   constructor(private authAdmin: AdminService,
               private router:Router) { }
-=======
-  constructor( private authAdmin: AdminService
-             , @Inject(Router) private router: Router
-               ) { }
-               
->>>>>>> 0b314cf0102226bcd635a8ac119f3cd660fd5eb4
   @Output() openAndClose: EventEmitter<any> = new EventEmitter();
   ngOnInit(): void {
   }
