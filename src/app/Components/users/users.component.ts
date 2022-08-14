150cb5cd9c208bb9175828fa27187308b288feaa
ahmed <89364017+Ahmedhassanin12@users.noreply.github.com>
2022-08-15 00:47:47 +0200

    upd component users

--- src/app/Components/users/users.component.ts
+++ src/app/Components/users/users.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Seller } from 'app/Models/seller';
+import { UserService } from 'app/Services/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
-
-  constructor( ) { }
+  searchText: string = '';
+  usersList: Seller[] | any;
+  orderList: [] | any;
+  constructor(private users: UserService) {}
 
   ngOnInit(): void {
+    this.users.getAllUsers().subscribe((data) => {
+      this.usersList = data.map((s) => {
+        return {
+          id: s.payload.doc.id,
+          ...s.payload.doc.data(),
+        };
+      });
+    });
   }
-
 }
--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,30 +1,31 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+// import { environment } from 'src/environments/environment';
 
-import {addDoc,
-       getDoc,
-       getDocs,
-       deleteDoc,
-       updateDoc ,
-       collection,
-       getFirestore} from "@angular/fire/firestore";
-import {AngularFirestore , AngularFirestoreCollection} from "@angular/fire/compat/firestore"
+import {
+  addDoc,
+  getDoc,
+  getDocs,
+  deleteDoc,
+  updateDoc,
+  collection,
+  getFirestore,
+} from '@angular/fire/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 
 import { Seller } from '../Models/seller';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Database } from '@angular/fire/database';
+import { CRUDService } from './crud.service';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
-
-  constructor(
-              ) {
-
+  constructor(private crud:CRUDService) {}
+  getAllUsers(){
+      return this.crud.getAll("users")
   }
-  
-
-
 }
